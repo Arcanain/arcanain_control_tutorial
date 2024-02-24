8487eb144eed0f38b34678838643a598916bc1c0
Ramune6110 <arduino6110@gmail.com>
2024-02-25 03:10:29 +0900

    [add]obstacle_avoidance

---
+++ launch/control_tutorial.py
@@ -0,0 +1,42 @@
+from launch import LaunchDescription
+from launch.substitutions import PathJoinSubstitution
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    package_name = 'arcanain_control_tutorial'
+    rviz_file_name = "arcanain_control_tutorial.rviz"
+
+    rviz_config_file = PathJoinSubstitution(
+        [FindPackageShare(package_name), "rviz", rviz_file_name]
+    )
+
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=["-d", rviz_config_file],
+    )
+
+    single_obstacle_avoidance_car_mpc_control_node = Node(
+        package=package_name,
+        executable='single_obstacle_avoidance_car_mpc_control',
+        output="screen",
+    )
+
+    multiple_obstacle_avoidance_car_mpc_control_node = Node(
+        package=package_name,
+        executable='multiple_obstacle_avoidance_car_mpc_control',
+        output="screen",
+    )
+
+    nodes = [
+        rviz_node,
+        single_obstacle_avoidance_car_mpc_control_node,
+        multiple_obstacle_avoidance_car_mpc_control_node,
+    ]
+
+    return LaunchDescription(nodes)
